6409f7d0357ebbef9ffecbb1bea1e9a629e0d985
danilo <102251125+dkjacksfdf@users.noreply.github.com>
2023-04-24 15:46:24 -0300

    crud pronto

--- server/routes/frete.js
+++ server/routes/frete.js
@@ -37,9 +37,42 @@ knl.get("frete", async (req, resp) => {
 knl.get("frete/:id", async (req, resp) => {
     let result = await knl.sequelize().models.frete.findAll({
         where: {
-            id: req.params.id
+            id: req.params.id,
+            status: 1
         }
     });
     resp.json(result);
 })
 
+knl.put("frete", async (req, resp) => {
+
+    let TotalValor = req.body.ValorTotal * req.body.desconto / 100
+
+    let result = await knl.sequelize().models.frete.update({
+        ValorTotal: TotalValor,
+        ativo: req.body.ativo,
+        valor: req.body.ValorTotal,
+        fkProduto: req.body.fkProduto,
+        fkEndereco: req.body.fkEndereco,
+        desconto: req.body.desconto,
+    }, {
+        where: {
+            id: req.body.id,
+            status: 1
+        }
+    });
+
+    resp.json({ "Status": "ok" })
+})
+
+knl.patch("frete", async (req, resp) => {
+    let result = await knl.sequelize().models.frete.update({
+        status:0
+    },{
+        where: {
+            id: req.body.id,
+        }
+    });
+
+    resp.json({ "Status": "ok" })
+})
