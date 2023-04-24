8ebd26658694fef967dceae5845060b0d429984a
eduardaramalho <eduardacns.ramalho@gmail.com>
2023-04-24 13:44:15 -0300

    initial commit

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,7 +1,29 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { UserComponent } from './user/user.component';
+import { AuthGuard } from './_helpers';
+import { Role } from './_models';
+import { ClientComponent } from './client/client.component';
+import { GroupComponent } from './group/group.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+//   {
+//     path: 'user',
+//     component: UserComponent,
+//     // canActivate: [AuthGuard],
+//     data: { roles: [Role.Admin]}
+// } ,
+// {
+//     path: 'client',
+//     component: ClientComponent,
+//     // canActivate: [AuthGuard]
+// },
+// {
+//     path: 'group',
+//     component: GroupComponent,
+//     // canActivate: [AuthGuard]
+// },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- client/src/app/menu-itens.ts
+++ client/src/app/menu-itens.ts
@@ -5,47 +5,62 @@ import { CollectionComponent } from "./collection/collection.component";
 import { ProductComponent } from "./product/product.component";
 import { ClientComponent } from "./client/client.component";
 import { SaleComponent } from "./sale/sale.component";
+
 export const MenuItens = [
     {
         path: 'user',
         caption : 'Usuário',
         icon : 'person',
+        permissao : [1],
         component: UserComponent,
     } ,
     {
         path: 'client',
         caption : 'Cliente',
         icon : 'supervisor_account',
+        permissao : [1, 2],
         component: ClientComponent,
     },
     {
         path: 'group',
         caption : 'Grupo',
         icon : 'assessment',
+        permissao : [1, 2],
         component: GroupComponent,
     },
     {
         path: 'subgroup',
         caption : 'Subgrupo',
         icon : 'assignment',
+        permissao : [1, 2],
         component: SubgroupComponent,
     },
     {
         path: 'collection',
         caption : 'Coleção',
         icon : 'next_week',
+        permissao : [1, 2],
         component: CollectionComponent,
     },
     {
         path: 'product',
         caption : 'Produto',
         icon : 'folder_special',
+        permissao : [1, 2, 3],
         component: ProductComponent,
     },
     {
         path: 'sale',
         caption : 'Pedido de venda',
         icon : 'add_shopping_cart',
+        permissao : [1, 4],
+        component: SaleComponent,
+    },
+    {
+        path: 'frete',
+        caption: 'Cálculo de fretes',
+        icon: 'add_shopping_cart',
+        permissao: [4],
         component: SaleComponent,
     }
 ]
