caf0f261a47893c563c86a13518bce3897603ca5
eduardaramalho <eduardacns.ramalho@gmail.com>
2023-05-15 15:00:06 -0300

    fix: duplicated code

--- server/models/frete-models.js
+++ server/models/frete-models.js
@@ -30,30 +30,3 @@ module.exports = (sequelize) => {
         },
     })
 }
-
-module.exports = (sequelize) => {
-    sequelize.define('frete', {
-        id : {
-            type : Sequelize.INTEGER.UNSIGNED,
-            primaryKey : true,
-            autoIncrement : true,
-            allowNull : false
-        },
-        status:{
-            type:Sequelize.INTEGER.UNSIGNED,
-            allowNull:false
-        },
-        ValorTotal:{
-            type : Sequelize.DECIMAL(10, 2),
-            allowNull : false,
-        },
-        desconto:{
-            type:Sequelize.INTEGER.UNSIGNED,
-            allowNull:false
-        },
-        ativo:{
-            type: Sequelize.STRING(200),
-            allowNull: false
-        }
-    })
-}
